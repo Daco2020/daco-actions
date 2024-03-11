1555e6d6185ef70946661f4d1ff2811b710656c1
Daco2020 <dacokim32@gmail.com>
2024-03-11 22:52:56 +0900

    feat: 리마인드 스크립트 추가

---
+++ remind.py
@@ -0,0 +1,13 @@
+import os
+
+import requests
+
+def send_message(message: str) -> None:
+    """메시지를 디스코드로 보낸다."""
+    requests.post(
+        os.environ["DISCORD_WEBHOOK_URL"],
+        data={"content": message},
+    )
+
+if __name__ == "__main__":
+    a = send_message("오늘 하루 배운 것을 올려보아요!🤓 <@937996864207331380>")
